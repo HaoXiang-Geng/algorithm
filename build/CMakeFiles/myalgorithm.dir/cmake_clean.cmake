3200d39ce477b90f0abe35f213989e564bc66d8d
root <1052576892@qq.com>
2023-05-31 09:31:40 +0000

    vscode debug

---
+++ build/CMakeFiles/myalgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myalgorithm.dir/main.cpp.o"
+  "myalgorithm.pdb"
+  "myalgorithm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myalgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
